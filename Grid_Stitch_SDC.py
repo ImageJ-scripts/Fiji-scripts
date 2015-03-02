cbf249b4446b316546491070ac14076ea18308c7
Daniel Matthews <dr.dan.matthews@gmail.com>
2015-03-02 15:16:38 +1000

    write channels separately and stitch separately for large datasets

--- Grid_Stitch_SDC.py
+++ Grid_Stitch_SDC.py
@@ -35,45 +35,59 @@ def delete_tiles(tiles_dir):
 
 def write_fused(output_path,meta):
 	imp = ij.WindowManager.getCurrentImage()
+	planes = imp.getStack()
+	
 	meta.setPixelsSizeX(PositiveInteger(imp.getWidth()),0)
 	meta.setPixelsSizeY(PositiveInteger(imp.getHeight()),0)
 	writer = ImageWriter()
 	writer.setCompression('LZW')
 	writer.setMetadataRetrieve(meta)
-	writer.setId("%sfused.ome.tif"%output_path)
+	file_path = "%sfused.ome.tif"%output_path
+	if os.path.exists(file_path):
+		os.remove(file_path)
+	writer.setId(file_path)
+	print writer.getFormat()
 	littleEndian = not writer.getMetadataRetrieve().getPixelsBinDataBigEndian(0, 0)
-	planes = imp.getStack()
+	
 	for p in range(planes.getSize()):
 		proc = planes.getProcessor(p+1)
 		writer.saveBytes(p,DataTools.shortsToBytes(proc.getPixels(), littleEndian))
 	writer.close()
 	
-def run_stitching(tiles_dir,gridX, gridY):
+def run_stitching(tiles_dir,tile_name,gridX, gridY):
 	IJ.run("Grid/Collection stitching", "type=[Grid: snake by rows] order=[Right & Down                ] "\
 			"grid_size_x=%s grid_size_y=%s tile_overlap=20 first_file_index_i=0 "\
-			"directory=[%s] file_names=tile_{i}.ome.tif "\
+			"directory=[%s] file_names=%s.ome.tif "\
 			"output_textfile_name=TileConfiguration.txt fusion_method=[Linear Blending] "\
 			"regression_threshold=0.30 max/avg_displacement_threshold=2.50 "\
 			"absolute_displacement_threshold=3.50 compute_overlap "\
-			"computation_parameters=[Save memory (but be slower)] "%(gridX,gridY,tiles_dir))
-
-
+			"computation_parameters=[Save memory (but be slower)] "%(gridX,gridY,tiles_dir,tile_name))
+	
 def write_tiles(r,tiles_dir,theT,sizeC,sizeZ,meta):
-	writer = ImageWriter()
-	writer.setCompression('LZW')
-	writer.setMetadataRetrieve(meta)
-	filename = "%stile_%s.ome.tif"%(tiles_dir,theT)
-	writer.setId(filename)
+	
 	planes = sizeZ * sizeC
-	p = 0
-	for theZ in range(sizeZ):
+	outputfiles = []
+	if planes > 100:
+		# write out the channels separately and then stitch separately
 		for theC in range(sizeC):
-			writer.saveBytes(p,r.openBytes(r.getIndex(theZ, theC, theT)))
-			p += 1
-	writer.close()
-	IJ.log("Writing tile %s: %s"%(theT,filename))
+			outputfiles.append("%stile_%s_channel_%s.ome.tif"%(tiles_dir,theT,theC))
+	else:
+		outputfiles.append("%stile_%s.ome.tif"%(tiles_dir,theT))
 
-def set_metadata(inputMeta,outputMeta):
+	p = 0
+	for theF in range(len(outputfiles)):
+		IJ.log("Writing tile %s: %s"%(theT,outputfiles[theF]))
+		writer = ImageWriter()
+		writer.setCompression('LZW')
+		writer.setMetadataRetrieve(meta)
+		writer.setId(outputfiles[theF])
+		for theC in range(sizeC):
+			for theZ in range(sizeZ):
+				writer.saveBytes(p,r.openBytes(r.getIndex(theZ, theC, theT)))
+				p += 1
+		writer.close()
+
+def set_metadata(inputMeta,outputMeta,channels):
 
 	outputMeta.setImageID("Image:0", 0)
 	outputMeta.setPixelsID("Pixels:0", 0)
@@ -92,12 +106,12 @@ def set_metadata(inputMeta,outputMeta):
 	sizeZ = inputMeta.getPixelsSizeZ(0).getValue()
 	sizeC = inputMeta.getPixelsSizeC(0).getValue()
 	sizeT = inputMeta.getPixelsSizeT(0).getValue()
-	for c in range(sizeC):
-		outputMeta.setChannelID("Channel:0:" + str(c), 0, c);
-		spp = inputMeta.getChannelSamplesPerPixel(0,c)
-		outputMeta.setChannelSamplesPerPixel(spp, 0, c);
-		name = inputMeta.getChannelName(0,c)
-		color = inputMeta.getChannelColor(0,c)
+	for c,chan in enumerate(channels):
+		outputMeta.setChannelID("Channel:0:" + str(c), 0, c)
+		spp = chan['spp']
+		outputMeta.setChannelSamplesPerPixel(spp, 0, c)
+		name = chan['name']
+		color = chan['color']
 		outputMeta.setChannelName(name,0,c)
 		outputMeta.setChannelColor(color,0,c)
 	
@@ -116,26 +130,61 @@ def run_script(input_dir,gridX,gridY):
 	input_path = glob.glob("%s*.tiff"%input_dir)[0]
 	sep = os.path.sep
 	inputMeta = MetadataTools.createOMEXMLMetadata()
-	outputMeta = MetadataTools.createOMEXMLMetadata()
 	reader = get_reader(input_path,inputMeta)
-	outputMeta = set_metadata(inputMeta,outputMeta)
-
+	
 	tiles_dir = os.path.join(input_dir,"tiles%s"%sep)
-	if not os.path.exists(tiles_dir):
-		os.makedirs(tiles_dir)
-		sizeZ = inputMeta.getPixelsSizeZ(0).getValue()
-		sizeC = inputMeta.getPixelsSizeC(0).getValue()
-		sizeT = inputMeta.getPixelsSizeT(0).getValue()
-		for theT in range(sizeT):
-			write_tiles(reader,tiles_dir,theT,sizeC,sizeZ,outputMeta)
-		last_tile = tiles_dir + 'tile_%s.ome.tif'%(sizeT-1)
+	os.makedirs(tiles_dir)
+	sizeZ = inputMeta.getPixelsSizeZ(0).getValue()
+	sizeC = inputMeta.getPixelsSizeC(0).getValue()
+
+	channels = []
+	chan_d = {}
+	for c in range(sizeC):
+		chan_d['spp'] = inputMeta.getChannelSamplesPerPixel(0,c)
+		chan_d['name'] = inputMeta.getChannelName(0,c)
+		chan_d['color'] = inputMeta.getChannelColor(0,c)
+		channels.append(chan_d)
+
+	planeCount = sizeZ * sizeC
+	if planeCount > 100:
+		# write separate channels and stitch separately
+		for theC in range(sizeC):
+			outputMeta = MetadataTools.createOMEXMLMetadata()
+			channel_to_write = [channels[theC]]
+			set_metadata(inputMeta,outputMeta,channel_to_write)
+			sizeT = inputMeta.getPixelsSizeT(0).getValue()
+			for theT in range(sizeT):
+				write_tiles(reader,tiles_dir,theT,1,sizeZ,outputMeta)
+				
+		last_tile = tiles_dir + 'tile_%s_channel%s.ome.tif'%(sizeT-1,sizeC-1)
 		print last_tile
 		while not os.path.exists(last_tile):
    			time.sleep(1)
-	reader.close()
-	run_stitching(tiles_dir,gridX,gridY)
-	write_fused(input_dir,outputMeta)
-	delete_tiles(tiles_dir)
+   			
+		reader.close()
+		for theC in range(sizeC):
+			tile_name = "tile_{i}_channel_%s"%theC
+			run_stitching(tiles_dir,tile_name,gridX,gridY)
+			write_fused(input_dir,outputMeta)
+			delete_tiles(tiles_dir)
+		
+   	else:
+   		outputMeta = MetadataTools.createOMEXMLMetadata()
+   		set_metadata(inputMeta,outputMeta,channels)
+		sizeT = inputMeta.getPixelsSizeT(0).getValue()
+		for theT in range(sizeT):
+			write_tiles(reader,tiles_dir,theT,sizeC,sizeZ,outputMeta)
+				
+		last_tile = tiles_dir + 'tile_%s.ome.tif'%(sizeT-1)
+		print last_tile
+		while not os.path.exists(last_tile):
+   			time.sleep(1)   	
+   			
+		reader.close()
+		tile_name = "tile_{i}"
+		run_stitching(tiles_dir,tile_name,gridX,gridY)
+		write_fused(input_dir,outputMeta)
+		delete_tiles(tiles_dir)
 
 def make_dialog():
 
